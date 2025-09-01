c9572f7091386f05df5f1b5bb76ab065ee3b627e
Aarni-FangMayer <aarni.fangmayer@gmail.com>
2025-09-02 02:37:01 +0300

    test: add interaction test for Note component button click handler

--- introdemo/src/components/Note.test.jsx
+++ introdemo/src/components/Note.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Note from './Note'
 
 test('renders content', () => {
@@ -7,8 +8,13 @@ test('renders content', () => {
     important: true
   }
 
-  render(<Note note={note} />)
+  const mockHandler = vi.fn()
 
-  const element = screen.getByText('Component testing is done with react-testing-library')
-  expect(element).toBeDefined()
+  render(<Note note={note} toggleImportance={mockHandler} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('make not important')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
